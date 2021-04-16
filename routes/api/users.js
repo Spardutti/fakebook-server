6aee97b5c69b0203a59603b05505f87191e093c7
Spardutti <luisdamian.sp@gmail.com>
2021-04-16 17:14:26 -0300

    added images to public folder

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./images");
+    cb(null, "./public/images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "." + file.originalname);
